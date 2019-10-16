123b7d6a777367a3fb40cfc929e012450c971646
Ig0rD <34953521+Ig0rD@users.noreply.github.com>
2019-10-16 10:32:04 -0300

    Add files via upload

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -19,14 +19,6 @@ self.addEventListener("install", function (event) {
                     './index.html',
                     'offline.html',
                     './manifest.json',
-                    './images/icons/icon-72x72.png',
-                    './images/icons/icon-96x96.png',
-                    './images/icons/icon-128x128.png',
-                    './images/icons/icon-144x144.png',
-                    './images/icons/icon-152x152.png',
-                    './images/icons/icon-192x192.png',
-                    './images/icons/icon-384x384.png',
-                    './images/icons/icon-512x512.png',
                     './pwabuilder-sw.js'
                 ]);
             }
